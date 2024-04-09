541c9c428c57da4ffb132e4d30638be84e108e1f
Josh Hed <josh.hed@chrobinson.com>
2024-04-09 17:25:56 -0500

    website commit

---
+++ theheds/emily/emily2/helper.js
@@ -0,0 +1,11 @@
+function viewPicture( picture )
+{
+  /*
+  alert( 'We are currently experiencing some technical problems.  '     +
+         'You will be unable to view the full size pictures until '     +
+         'further notice.  An email will be sent out when the problem ' +
+         'has been resolved.  Sorry for the inconvenience.' );
+  */
+
+    window.open( '../pics/' + picture, '_blank' );
+}
